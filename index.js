ee5849953c256d8edc6089a524a931fd68386593
arine <ariangerris@gmail.com>
2024-07-22 21:23:25 +0400

    user auth implemented

---
+++ index.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("loginForm");
+  const loginFormSection = document.getElementById("loginFormSection");
+  const mainPage = document.getElementById("mainPage");
+  const logoutButton = document.getElementById("logoutButton");
+  const invoicesTable = document
+    .getElementById("invoicesTable")
+    .getElementsByTagName("tbody")[0];
+  loginForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const usersResponse = await fetch(
+      "https://bever-aca-assignment.azurewebsites.net/Users"
+    );
+    const usersData = await usersResponse.json();
+    const users = usersData.value;
+    const user = users.find(
+      (user) => user.Name === username && user.Password === password
+    );
+    if (user) {
+      loginFormSection.style.display = "none";
+      mainPage.style.display = "block";
+      await fetchInvoices(user.UserId);
+    } else {
+      alert("Invalid username or password");
+    }
+  });
+
+  logoutButton.addEventListener("click", function () {
+    mainPage.style.display = "none";
+    loginFormSection.style.display = "block";
+    invoicesTable.innerHTML = "";
+  });
+});
