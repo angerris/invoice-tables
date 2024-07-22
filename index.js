59b3c3f4c5b43d70933fb23ed5a995d10dc86816
arine <ariangerris@gmail.com>
2024-07-22 21:54:18 +0400

    invoices data fetch

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const invoicesTable = document
     .getElementById("invoicesTable")
     .getElementsByTagName("tbody")[0];
+
   loginForm.addEventListener("submit", async function (event) {
     event.preventDefault();
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-
     const usersResponse = await fetch(
       "https://bever-aca-assignment.azurewebsites.net/Users"
     );
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (user) {
       loginFormSection.style.display = "none";
       mainPage.style.display = "block";
+      logoutButton.style.display = "block";
       await fetchInvoices(user.UserId);
     } else {
       alert("Invalid username or password");
@@ -33,4 +34,22 @@ document.addEventListener("DOMContentLoaded", function () {
     loginFormSection.style.display = "block";
     invoicesTable.innerHTML = "";
   });
+
+  async function fetchInvoices(userId) {
+    const invoicesResponse = await fetch(
+      "https://bever-aca-assignment.azurewebsites.net/Invoices"
+    );
+    const invoicesData = await invoicesResponse.json();
+    const userInvoices = invoicesData.value.filter(
+      (invoice) => invoice.UserId === userId
+    );
+
+    userInvoices.forEach((invoice) => {
+      const row = invoicesTable.insertRow();
+      row.insertCell(0).innerText = invoice.Name;
+      row.insertCell(1).innerText = new Date(
+        invoice.PaidDate
+      ).toLocaleDateString();
+    });
+  }
 });
